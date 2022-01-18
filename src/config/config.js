b707d095603b9cfd02c2079fdfd45d1acfb8023b
Adarsh vk <adarshvk1997@yahoo.com>
2022-01-18 21:07:25 +0530

    remove api keys

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: "mongodb://localhost:27017/my_database",
     secret: 'supersecret',
-    openWeatherAPIKey: "19425eeb7123a1ca89567f7ab95fb18d", // Add your open Weather API Key
+    openWeatherAPIKey: "", // Add your open Weather API Key
     sendGridAPIKey: "" // Add your send Grid API Key
 }
